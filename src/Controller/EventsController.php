7b9fcc46f623b8521cd5617ddc7f2ab9b706c307
TamaraZolnajic <zolnajic.tamara@gmail.com>
2022-08-06 21:08:45 +0200

    second commit

---
+++ src/Controller/EventsController.php
@@ -0,0 +1,149 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use App\Service\FileUploader;
+use App\Repository\EventsRepository;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
+use App\Entity\Location;
+use App\Entity\Events;
+use App\Form\EventsType;
+use Symfony\Contracts\EventDispatcher\Event;
+
+class EventsController extends AbstractController
+{
+    #[Route('/', name: 'events')]
+    public function index(ManagerRegistry $doctrine): Response
+    {
+        $events = $doctrine->getRepository(Events::class)->findAll();
+        return $this->render('events/index.html.twig', [
+            'controller_name' => 'EventsController', "events" => $events
+        ]);
+    }
+    #[Route('/create', name: 'create')]
+    public function create(Request $request, EventsRepository $eventsRepository, ManagerRegistry $doctrine, FileUploader $fileUploader): Response
+    {
+        $event = new Events();
+        $form = $this -> createForm(EventsType::class, $event);
+
+        $form -> handleRequest($request);
+        if ($form -> isSubmitted() && $form -> isValid()) {
+
+            $pictureFile = $form->get('picture')->getData();
+            if($pictureFile){
+                $pictureFileName = $fileUploader->upload($pictureFile);
+                $event->setPicture($pictureFileName);
+            }
+            // $now = new \DateTime('now');
+            $event = $form -> getData();
+            $em = $doctrine -> getManager();
+            $em -> persist($event);
+            // $location = $doctrine -> getRepository(Location::class)->find(1);
+            // $event -> setFkLocation($location);
+            $em -> flush();
+
+            $eventsRepository -> add($event);
+            $this -> addFlash(
+                'notice',
+                'Event Added'
+            );
+            return $this -> redirectToRoute('events');
+        }
+
+        return $this->render('events/create.html.twig', [
+            'form' => $form->createView()
+        ]);
+
+
+
+
+
+    }#[Route('/edit/{id}', name: 'edit')]
+    public function edit(Request $request, ManagerRegistry $doctrine, $id): Response
+    {
+        $event = $doctrine -> getRepository(Events::class) -> find($id);
+        $form = $this -> createForm(EventsType::class, $event);
+
+        $form -> handleRequest($request);
+
+        if($form -> isSubmitted() && $form -> isValid()) {
+
+            $event = $form -> getData();
+            $em = $doctrine -> getManager();
+            $em -> persist($event);
+            $em -> flush();
+            $this -> addFlash(
+                'notice',
+                'Event Edited'
+            );
+            return $this -> redirectToRoute('events');
+        }
+        return $this->render('events/edit.html.twig', [
+            'form' => $form->createView()
+        ]);
+
+    }#[Route('/details/{id}', name: 'details')]
+    public function details(ManagerRegistry $doctrine, $id): Response
+    {
+        $event = $doctrine -> getRepository(Events::class) -> find($id);
+        // dd($event);
+        return $this->render('events/details.html.twig', [
+            'controller_name' => 'EventsController', "event" => $event
+        ]);
+    }
+    #[Route('/delete/{id}', name: 'delete')]
+  public function delete ($id, ManagerRegistry $doctrine){
+      $em = $doctrine -> getManager();
+      $event = $em->getRepository(Events::class)->find($id);
+      $em->remove($event);
+
+      $em->flush();
+      $this->addFlash(
+          'notice',
+          'Event Removed'
+      );
+      return $this->redirectToRoute('events');
+}
+#[Route('/findby/{type}', name: 'findby')]
+    public function findby(ManagerRegistry $doctrine, $type): Response
+    {
+        $repository = $doctrine -> getRepository(Events::class);
+        $event = $repository -> findby(['type' => $type]);
+        return $this->render('events/findby.html.twig', [
+            'controller_name' => 'EventsController', "event" => $event
+        ]);
+    }
+    #[Route('/about', name: 'about')]
+   public function about(): Response
+   {
+       return $this->render('events/about.html.twig', [
+           'controller_name' => 'EventsController',
+       ]);
+   } 
+   #[Route('/home', name: 'home')]
+   public function home(): Response
+   {
+       return $this->render('events/home.html.twig', [
+           'controller_name' => 'EventsController',
+       ]);
+   } 
+   #[Route('/contact', name: 'contact')]
+   public function contact(): Response
+   {
+       return $this->render('events/contact.html.twig', [
+           'controller_name' => 'EventsController',
+       ]);
+   }
+   
+}
---
+++ src/Entity/Events.php
@@ -0,0 +1,186 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EventsRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EventsRepository::class)]
+class Events
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $date = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $photo = null;
+
+    #[ORM\Column]
+    private ?int $capacity = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $email = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $phone = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $adress = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $city = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $url = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $type = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPhoto(): ?string
+    {
+        return $this->photo;
+    }
+
+    public function setPhoto(string $photo): self
+    {
+        $this->photo = $photo;
+
+        return $this;
+    }
+
+    public function getCapacity(): ?int
+    {
+        return $this->capacity;
+    }
+
+    public function setCapacity(int $capacity): self
+    {
+        $this->capacity = $capacity;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPhone(): ?int
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(?int $phone): self
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+
+    public function getAdress(): ?string
+    {
+        return $this->adress;
+    }
+
+    public function setAdress(string $adress): self
+    {
+        $this->adress = $adress;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): self
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getUrl(): ?string
+    {
+        return $this->url;
+    }
+
+    public function setUrl(string $url): self
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+}
